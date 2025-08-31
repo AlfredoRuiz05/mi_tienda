c1e061aab1269b477e71e6fb9383189f15fc4a40
AlfredoRuiz05 <ruizalfredo512@gmail.com>
2025-08-31 13:39:59 -0300

    fix: agregar __init__.py en apps para que Django lo reconozca

--- tienda_lr/tienda_lr/settings.py
+++ tienda_lr/tienda_lr/settings.py
@@ -76,17 +76,25 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tienda_lr',
-        'USER': 'postgres',
-        'PASSWORD':'05072001',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'tienda_lr',
+#        'USER': 'postgres',
+#        'PASSWORD':'05072001',
+#        'HOST': 'localhost',
+#        'PORT': 5432
+#    }
+#}
 
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
