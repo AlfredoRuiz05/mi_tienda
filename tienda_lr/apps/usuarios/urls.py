a98d59efbafa11ece4b69951cb47ebf3f5922de1
AlfredoRuiz05 <ruizalfredo512@gmail.com>
2025-09-02 03:27:17 -0300

    .

--- tienda_lr/apps/usuarios/urls.py
+++ tienda_lr/apps/usuarios/urls.py
@@ -2,7 +2,7 @@
 # apps/usuarios/urls.py
 from django.urls import path
 from . import views
-
+app_name = "usuarios"
 urlpatterns = [
     path('login/', views.iniciar_sesion, name='iniciar_sesion'),
     path('registrarse/', views.registrar_usuario, name='registrarse'),
