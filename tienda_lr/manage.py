abb092a882c309c8a5d39f58348381b99027a951
AlfredoRuiz05 <ruizalfredo512@gmail.com>
2025-09-02 03:06:47 -0300

    Modificar manage.py para agregar ruta al PYTHONPATH y arreglar ModuleNotFoundError

--- tienda_lr/manage.py
+++ tienda_lr/manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+# Agregar la carpeta raíz al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
+
