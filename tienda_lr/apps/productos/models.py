885e7a0a0d286d4a2731593c74938e9f0ebb1151
AlfredoRuiz05 <ruizalfredo512@gmail.com>
2025-09-06 18:34:14 -0300

    modificaciones para cloudinary

--- tienda_lr/apps/productos/models.py
+++ tienda_lr/apps/productos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     descuento = models.PositiveIntegerField(default=0)
-    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+    imagen = CloudinaryField('imagen', blank=True, null=True)
 
     def __str__(self):
         return self.nombre
--- tienda_lr/core/settings.py
+++ tienda_lr/core/settings.py
@@ -67,9 +67,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Base de datos
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # 👉 Render (Postgres en la nube)
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    }
+else:
+    # 👉 Local (SQLite para pruebas)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Contraseñas
 AUTH_PASSWORD_VALIDATORS = [
