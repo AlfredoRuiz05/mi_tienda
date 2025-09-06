a7fdd7408668878a7856c404a29cc10d3233182d
AlfredoRuiz05 <ruizalfredo512@gmail.com>
2025-09-06 02:36:00 -0300

    admi de prod y usua

--- tienda_lr/apps/usuarios/admin.py
+++ tienda_lr/apps/usuarios/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario)
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = Usuario
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permisos', {'fields': ('is_staff', 'is_active')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('username',)
+
+admin.site.register(Usuario, CustomUserAdmin)
